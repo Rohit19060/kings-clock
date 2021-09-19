02b20172dbc2474a29dda20959436df9e1210d24
Rohit Jain <rohitjain19060@gmail.com>
2021-09-19 08:18:40 +0530

    WIdget Const Added

--- kings_clock/test/widget_test.dart
+++ kings_clock/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:kings_clock/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(DigitalClock());
+    await tester.pumpWidget(const DigitalClock());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
