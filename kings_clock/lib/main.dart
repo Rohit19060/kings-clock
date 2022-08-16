5d8f08b5a2b4ea4fd277640c9a5dd0649d9426f0
Rohit Jain <rohitjain19060@gmail.com>
2022-08-16 23:42:01 +0530

    👑Updated Code to Latest Flutter & Gradle with Material 3

--- kings_clock/lib/main.dart
+++ kings_clock/lib/main.dart
@@ -1,33 +1,38 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:wakelock/wakelock.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
-    SystemUiOverlay.top,
-    SystemUiOverlay.bottom,
-  ]);
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.landscapeRight, DeviceOrientation.landscapeLeft]);
-  runApp(
-    const DigitalClock(),
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [
+      SystemUiOverlay.top,
+      SystemUiOverlay.bottom,
+    ],
   );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
+  runApp(const DigitalClock());
 }
 
 class DigitalClock extends StatefulWidget {
-  const DigitalClock({Key? key}) : super(key: key);
+  const DigitalClock({super.key});
 
   @override
-  _DigitalClockState createState() => _DigitalClockState();
+  State<DigitalClock> createState() => _DigitalClockState();
 }
 
 class _DigitalClockState extends State<DigitalClock> {
   @override
   void initState() {
     super.initState();
+    Wakelock.enable();
     Timer.periodic(const Duration(seconds: 1), (V) {
       if (mounted) {
         setState(() {
@@ -41,125 +46,98 @@ class _DigitalClockState extends State<DigitalClock> {
     });
   }
 
-  _hour0() {
-    var hour = DateTime.now().hour.toInt() / 10;
-    return "${hour.toInt()}";
-  }
+  String _hour0() => '${DateTime.now().hour ~/ 10}';
 
-  _hour1() {
-    var hour = DateTime.now().hour.toInt() % 10;
-    return "$hour";
-  }
+  String _hour1() => '${DateTime.now().hour % 10}';
 
-  _minute0() {
-    var min = DateTime.now().minute.toInt() / 10;
-    return "${min.toInt()}";
-  }
+  String _minute0() => '${DateTime.now().minute ~/ 10}';
 
-  _minute1() {
-    var min = DateTime.now().minute.toInt() % 10;
-    return "$min";
-  }
+  String _minute1() => '${DateTime.now().minute % 10}';
 
-  _date() {
-    var day = DateTime.now().day.toInt();
+  String _date() {
+    final now = DateTime.now();
     const monthNames = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
     ];
-    var month = monthNames[DateTime.now().month.toInt() - 1];
-    var year = DateTime.now().year.toInt();
-    return "$day $month, $year";
+    return '${now.day} ${monthNames[now.month - 1]}, ${now.year}';
   }
 
-  Widget handle(d) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15.0),
-      padding: const EdgeInsets.all(5),
-      width: 65.0,
-      height: 155.0,
-      decoration: BoxDecoration(
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 20.0,
-          )
-        ],
-        borderRadius: BorderRadius.circular(50.0),
-        gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Colors.black.withOpacity(0.2),
-              Colors.white.withOpacity(0.6),
-              Colors.white,
-              Colors.white.withOpacity(0.6),
-              Colors.black.withOpacity(0.2),
-            ]),
-      ),
-      child: FittedBox(
-        child: Center(
-          child: Text(
-            d,
-            style: const TextStyle(
-                color: Colors.black, fontWeight: FontWeight.bold),
+  Widget handle(String d) => Container(
+        margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 15.0),
+        padding: const EdgeInsets.all(5),
+        width: 65.0,
+        height: 155.0,
+        decoration: BoxDecoration(
+          boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 20.0)],
+          borderRadius: BorderRadius.circular(50.0),
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.black.withOpacity(0.2),
+                Colors.white.withOpacity(0.6),
+                Colors.white,
+                Colors.white.withOpacity(0.6),
+                Colors.black.withOpacity(0.2),
+              ]),
+        ),
+        child: FittedBox(
+          child: Center(
+            child: Text(d,
+                style: const TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.bold)),
           ),
         ),
-      ),
-    );
-  }
+      );
 
   @override
-  Widget build(BuildContext context) {
-    Wakelock.enable();
-    return MaterialApp(
-      theme: ThemeData(brightness: Brightness.light),
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              _date(),
-              style: const TextStyle(fontSize: 24),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                handle(_hour0()),
-                handle(_hour1()),
-                const Anime(),
-                handle(_minute0()),
-                handle(_minute1()),
-              ],
-            ),
-            const Text(
-              "Digital Clock",
-              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-            ),
-          ],
+  Widget build(BuildContext context) => MaterialApp(
+        theme: ThemeData(brightness: Brightness.light, useMaterial3: true),
+        darkTheme: ThemeData(brightness: Brightness.dark, useMaterial3: true),
+        home: Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                _date(),
+                style: const TextStyle(fontSize: 24),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  handle(_hour0()),
+                  handle(_hour1()),
+                  const Anime(),
+                  handle(_minute0()),
+                  handle(_minute1()),
+                ],
+              ),
+              const Text(
+                'Digital Clock',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
 }
 
 class Anime extends StatefulWidget {
-  const Anime({Key? key}) : super(key: key);
+  const Anime({super.key});
 
   @override
-  _AnimeState createState() => _AnimeState();
+  State<Anime> createState() => _AnimeState();
 }
 
 class _AnimeState extends State<Anime> with SingleTickerProviderStateMixin {
@@ -196,21 +174,23 @@ class _AnimeState extends State<Anime> with SingleTickerProviderStateMixin {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: AnimatedLogo(animation: animation),
-    );
+  Widget build(BuildContext context) =>
+      Center(child: AnimatedLogo(animation: animation));
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<AnimationController>(
+        'animationController', animationController));
+    properties
+        .add(DiagnosticsProperty<Animation<double>?>('animation', animation));
   }
 }
 
 class AnimatedLogo extends AnimatedWidget {
-  AnimatedLogo({Key? key, Animation? animation})
-      : super(key: key, listenable: animation!);
-  _sec() {
-    var sec1 = DateTime.now().second.toInt() / 10;
-    var sec0 = DateTime.now().second.toInt() % 10;
-    return "${sec1.toInt()}$sec0";
-  }
+  AnimatedLogo({super.key, Animation<double>? animation})
+      : super(listenable: animation!);
+  String _sec() =>
+      '${DateTime.now().second ~/ 10}${DateTime.now().second % 10}';
 
   @override
   Widget build(BuildContext context) {
